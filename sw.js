018e7da6a8088ff650943198f5aca87ddafa3120
Pierpaolo Serrano <pierpaolo.serrano@accuratesolutions.it>
2026-07-15 16:59:32 +0200

    Uploading test website travel japan

---
+++ sw.js
@@ -0,0 +1,8 @@
+﻿const C='giappone-v1';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
+  self.skipWaiting();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
